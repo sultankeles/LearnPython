cc9ce09227c25b9209a1456b4916443a24c8ef7f
Sultan Keles <115416147+sultankeles@users.noreply.github.com>
2023-09-12 00:22:56 +0300

    Add files via upload

---
+++ textForm.py
@@ -0,0 +1,163 @@
+# Print is a function used to display content on the screen when we want to print something.
+print("Hello World!!")
+
+
+a = 5
+if a == 5:
+    print("a beşe eşittir") # The print function runs if the condition is met.
+
+
+''' This is a comment line. '''
+
+
+a = " # is used to add a comment line."
+b = " In end-of-line comments, you should leave 2 spaces before the # symbol and 1 space after the # symbol."
+c = " If we want to comment out a long piece of text, we should add three single quotation marks at the beginning and end of it. "
+
+print(len(a))
+print(len(b))
+print(len(c))
+
+number = 1
+
+# "1number = 2" and "1 = 3" are incorrect definitions.
+
+# İstanbul = 34" is not incorrect, but Turkish characters should be avoided.
+
+number_two = 3
+
+# "number four = 2" and "for = 6" are incorrect definitions.
+
+print(help("keywords"))
+
+# int number=5
+
+
+
+print("Hello Python")
+print("""Hello Python""")
+print('''Hello Python''')
+print('Hello Python')
+
+
+x = 1
+y = -121324353
+z = 0 
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+a = 35e5
+b = -4.134514541
+c = 9.5648313
+d = -45.454e3
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+f = 3+8j
+g = 6j
+s = -6j
+
+print(type(f))
+print(type(g))
+print(type(s))
+
+n = True
+m = False
+
+print(type(n))
+print(type(m))
+
+# If we convert a float variable to an integer, no rounding is performed.
+
+''' 
+i = 3.14       a = 3.98
+
+i = int(i)     a = int(a)
+
+Both expressions will yield an output of 3.
+
+'''
+
+age = 25
+weight = 50.5
+name = 'Sultan'
+isStudent = True
+
+print(type(age))
+print(type(weight))
+print(type(name))
+print(type(isStudent))
+
+print(isStudent)
+resault = str(age)
+print()
+print(resault)
+print()
+print(type(resault))
+
+
+a = 9
+a = 'Sultan'
+a = 8
+a = 'Pakize'
+
+
+
+a = 9
+b = 10
+print(a + b)
+a = 5
+print(a + b)
+a = 8
+print(a + b)
+a = 1
+print(a + b)
+
+a, b, c = 10, 20, 30
+
+print(a)
+print(b)
+print(c)
+
+
+x = y = z = "Pakize"
+
+print(x)
+print(y)
+print(z)
+
+a = "p"
+b = "i"
+c = "a"
+print(a + b + c)
+
+print(500 + (500 * 0.18))
+print(500 + (500 * 0.18))
+print(500 + (500 * 0.18))
+print(500 + (500 * 0.18))
+print(500 + (500 * 0.18))
+print(500 + (500 * 0.18))
+print(500 + (500 * 0.18))
+print(500 + (500 * 0.18))
+print(500 + (500 * 0.18))
+
+# To prevent wasting time, we should write this by assigning it to a variable.
+
+pa = 800
+kdv = 0.20
+
+print(pa + (pa * kdv))
+print(pa + (pa * kdv))
+print(pa + (pa * kdv))
+print(pa + (pa * kdv))
+print(pa + (pa * kdv))
+print(pa + (pa * kdv))
+print(pa + (pa * kdv))
+print(pa + (pa * kdv))
+print(pa + (pa * kdv))
+
